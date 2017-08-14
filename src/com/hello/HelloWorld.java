1a79335f7c11aa23ee890c14282bc4c0d215d3c6
pallab31 <pallab31@gmail.com>
2017-08-14 14:46:42 +0530

    Init Commit

---
+++ src/com/hello/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.hello;
+
+/**
+ * @author Pallab Paul
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
